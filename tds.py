fc0ef81b7a910f2b030a9e247543d2d35a7b8da1
aranyaiitm <131293050+aranyaiitm@users.noreply.github.com>
2023-04-19 23:23:08 +0530

    Create tds.py

---
+++ tds.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Find largest number among below three numbers")
+n1 = st.number_input("Enter the 1st number")
+n2 = st.number_input("Enter the 2nd number")
+n3 = st.number_input("Enter the 3rd number")
+
+def largest(n1,n2,n3):
+  if n1 > n2 and n1 > n3:
+    return n1
+  elif n2 > n1 and n2 > n3:
+    return n2
+  else:
+
+if calculate:
+  largestn = def largest(n1,n2,n3)
+  st.write("The largest number is: ",largestn)
